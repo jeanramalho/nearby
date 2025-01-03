0b7ccbaf98e9c7f096799a62a4e8bcbcf892095c
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-01-03 12:34:04 -0300

    [add] - Cria elementos e estrutura do componente

--- nearby/Sources/Features/Components/TipsView.swift
+++ nearby/Sources/Features/Components/TipsView.swift
@@ -9,6 +9,47 @@ import UIKit
 
 public class TipsView: UIView {
     private let iconImageView: UIImageView = {
-        
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFit
+        return image
     }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    init(icon: UIImage, title: String, description: String) {
+        super.init(frame: .zero)
+        setupComponent(icon: icon, title: title, description: description)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupComponent(icon: UIImage, title: String, description: String){
+        iconImageView.image = icon
+        titleLabel.text = title
+        descriptionLabel.text = description
+    }
+    
+    private func setupUI(){
+        setupConstraints()
+    }
+    
+    private func setupConstraints(){
+        
+    }
+    
+   
 }
