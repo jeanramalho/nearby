4e8a661c79b93463f8679917895bd692f0809d98
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-01-03 12:23:36 -0300

    [add] - inicia componentização da pagina de dicas

---
+++ nearby/Sources/Features/Components/TipsView.swift
@@ -0,0 +1,14 @@
+//
+//  TipsView.swift
+//  nearby
+//
+//  Created by Jean Ramalho on 03/01/25.
+//
+import Foundation
+import UIKit
+
+public class TipsView: UIView {
+    private let iconImageView: UIImageView = {
+        
+    }()
+}
