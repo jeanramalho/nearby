7c1331dfbcc4b9556d2cbea19f710e39162541ca
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-01-07 08:52:57 -0300

    [add] - Inicia a criação da homeView com o MapKit

--- nearby/Sources/Features/Scenes/HomeView/HomeView.swift
+++ nearby/Sources/Features/Scenes/HomeView/HomeView.swift
@@ -4,4 +4,26 @@
 //
 //  Created by Jean Ramalho on 06/01/25.
 //
+import Foundation
+import MapKit
 
+class HomeView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        setupConstraints()
+    }
+    
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+}
